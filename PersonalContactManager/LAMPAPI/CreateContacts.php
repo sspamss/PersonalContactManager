6566c6676a918237534e9e4de7d66f2877fb9346
Ritam Saha <82484605+riptam@users.noreply.github.com>
2023-02-02 00:02:33 -0500

    Add files via upload

---
+++ PersonalContactManager/LAMPAPI/CreateContacts.php
@@ -0,0 +1,45 @@
+<?php
+	$inData = getRequestInfo();
+	
+	-- obtaining all the User info
+	$firstName = $inData["firstName"];
+ 	$lastName = $inData["lastName"];
+	$phoneNumber = $inData["phoneNumber"];
+ 	$emailAddress = $inData["emailAddress"];
+ 	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+	/* creating a new contact based on the input provided */
+	if ($conn->connect_error)
+	{
+			returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (FirstName,LastName,PhoneNumber,EmailAddress, UserID) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("ssssi", $firstName, $lastName, $phoneNumber, $emailAddress, $userId);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
